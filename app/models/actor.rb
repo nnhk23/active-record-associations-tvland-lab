320497e417c7025654b96b2dd77788562e982ac3
Kim Nguyen <nnhk23@gmail.com>
2020-10-17 15:34:33 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        Character.find(1).name + " - " + Show.find(1).name
+    end
 end
